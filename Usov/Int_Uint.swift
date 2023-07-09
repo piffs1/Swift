1fa88ad83e0b34a2e2cbcb61ebde430251bd0e56
Nikita <nagafonov1@yandex.ru>
2023-07-09 11:56:37 +0300

    так получается Int вообще невыгодно использовать?

---
+++ Usov/Int_Uint.swift
@@ -0,0 +1,30 @@
+// Int data types
+
+// Int
+print("Int minimum is \(Int.min)") //  -9223372036854775808
+print("Int maximum is  \(Int.max)") //  9223372036854775807
+// Int8
+print("Int8 minimum is \(Int8.min)") // -128
+print("Int8 maximum is  \(Int8.max)") // 127
+//Int16
+print("Int16 minimum is \(Int16.min)") // -32768
+print("Int16 maximum is  \(Int16.max)") // 32767
+//Int32
+print("Int32 minimum is \(Int32.min)") // -2147483648
+print("Int32 maximum is  \(Int32.max)") //  2147483647
+//Int64 equals Int
+//Uint
+print("UInt minimum is \(UInt.min)") //  0
+print("UInt maximum is  \(UInt.max)") //  18446744073709551615
+//UInt8
+print("UInt8 minimum is \(UInt8.min)") // 0
+print("UInt8 maximum is  \(UInt8.max)") // 255
+//UInt16
+print("UInt16 minimum is \(UInt16.min)") // 0
+print("UInt16 maximum is  \(UInt16.max)") // 65535
+//UInt32
+print("UInt32 minimum is \(UInt32.min)") // 0
+print("UInt32 maximum is  \(UInt32.max)") //  4294967295
+//UInt64
+print("UInt32 minimum is \(UInt64.min)") // 0
+print("UInt32 maximum is  \(UInt64.max)") //  18446744073709551615
