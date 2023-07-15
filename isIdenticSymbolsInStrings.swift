8f209699dd08a3fb6fec78de318231d2a41a059c
Nikita <nagafonov1@yandex.ru>
2023-07-15 09:59:03 +0300

    [mod] fix two func in 1 file

--- isIdenticSymbolsInStrings.swift
+++ isIdenticSymbolsInStrings.swift
@@ -7,16 +7,3 @@ print(isIdenticSymbolsInStrings(in1:"AabbbbbB", in2:"BbabbbbA")) //true
 print(isIdenticSymbolsInStrings(in1:"RoFL", in2:"ROLL")) // false
 
 
-func countSymbol(text: String, symbol: Character) -> Int
-{
-    var countSymbols: Int = 0
-    for letter in text {
-        if letter == symbol {
-            countSymbols += 1
-        }
-    }
-    return countSymbols
-}
-
-print(countSymbol(text: "ABBCCC", symbol: "C")) /// 3
-
